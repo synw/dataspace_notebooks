178cc3d6c8e89cf4b57b1721737bc56f1b536c44
synw <synwx@protonmail.com>
2023-04-14 17:12:51 +0200

    Update notebooks

--- io/__init__.py
+++ io/__init__.py
@@ -3,4 +3,9 @@ from pathlib import Path
 
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
+#print("P", file.parents[2])
+#_dsl = file.parents[2] / "dataspace"
+#print("DSL", _dsl)
 sys.path.append(str(root))
+#sys.path.append(str(_dsl))
+#sys.path.append(str(_dsl / "dataspace"))
